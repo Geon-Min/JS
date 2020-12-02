d8171acdfe61ee1bd36aa3f0957211897cf92f3c
lkcbap12 <lkcbap12@naver.com>
2020-12-02 17:55:17 +0900

    커밋

---
+++ parc/popup.js
@@ -0,0 +1,27 @@
+function createCookie(name){
+    
+    //1. 쿠키생명주기
+    var date = new Date();
+    date.setDate(date.getDate() + 1); //1일 설정
+
+    //2. 쿠키형식 설정
+    var cookie = "";
+    cookie += name + "= ture;" ; //쿠키 이름, 값 설정
+    cookie += "expires=" + date.toUTCString(); //시간 설정
+    document.cookie = cookie; 
+
+}
+
+
+function getCookie(name){
+    var cookies = document.cookie.split("; "); //쿠키 자르기
+    //console.log(cookies);
+
+    //쿠키확인
+
+    for(var i in cookies){
+        if(cookies[i].search(name) != -1){ //쿠키를 탐색
+            return true;
+        }
+    }
+}
